c0be13ec2d71ff1d5045ea644ccbc7f4b3f1caa1
Emanuele <emanuele.vizin@gmail.com>
2018-08-06 12:36:41 +0200

    Service worker task in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 // generated on 2018-08-05 using generator-webapp 3.0.1
 const gulp = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins');
+const browserify = require('browserify');
+const babelify = require ('babelify');
+const source = require ('vinyl-source-stream');
 const browserSync = require('browser-sync').create();
 const del = require('del');
 const wiredep = require('wiredep').stream;
 const runSequence = require('run-sequence');
+
 
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
@@ -36,6 +40,18 @@ gulp.task('scripts', () => {
     .pipe(reload({stream: true}));
 });
 
+gulp.task('serviceWorker', () => {
+        const sw = browserify ({
+          debug: true
+        });
+        return sw
+        .transform(babelify)
+        .require("app/sw.js", {entry:true})
+        .bundle()
+        .pipe(source("sw.js"))
+        .pipe(gulp.dest(".tmp/"))
+});
+
 function lint(files) {
   return gulp.src(files)
     .pipe($.eslint({ fix: true }))
@@ -53,7 +69,7 @@ gulp.task('lint:test', () => {
     .pipe(gulp.dest('test/spec'));
 });
 
-gulp.task('html', ['styles', 'scripts'], () => {
+gulp.task('html', ['styles', 'scripts','serviceWorker'], () => {
   return gulp.src('app/*.html')
     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
     .pipe($.if(/\.js$/, $.uglify({compress: {drop_console: true}})))
@@ -95,7 +111,7 @@ gulp.task('extras', () => {
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
 gulp.task('serve', () => {
-  runSequence(['clean', 'wiredep'], ['styles', 'scripts', 'fonts'], () => {
+  runSequence(['clean', 'wiredep'], ['styles', 'scripts','serviceWorker', 'fonts'], () => {
     browserSync.init({
       notify: false,
       port: 9000,
@@ -114,6 +130,7 @@ gulp.task('serve', () => {
     ]).on('change', reload);
 
     gulp.watch('app/styles/**/*.scss', ['styles']);
+    gulp.watch('app/sw.js', ['serviceWorker']);
     gulp.watch('app/scripts/**/*.js', ['scripts']);
     gulp.watch('app/fonts/**/*', ['fonts']);
     gulp.watch('bower.json', ['wiredep', 'fonts']);
